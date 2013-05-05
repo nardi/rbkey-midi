70e6f403d45b9102e8f5dfa877ac67d61ec295f6
Nardi Lam <me@nardilam.nl>
2013-05-05 22:50:45 +0200

    Basic functionality, octave switching, and pitch bend/modulation using touch strip

---
+++ main.js
@@ -0,0 +1,95 @@
+var _ = require('underscore');
+var RBKeyboard = require('./rbkey');
+var midi = require('midi');
+var readline = require('readline');
+
+readline.open = function() {
+  return this.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+};
+
+// Lazy error handling
+process.on('uncaughtException', function(error) {
+  console.error('Error: ' + error.message);
+  process.exit();
+});
+
+console.log('Welcome to the Rock Band Keyboard to MIDI bridge version 0.1!');
+
+var kb = new RBKeyboard();
+var octave = 0;
+var octaveData = {};
+var output = new midi.output();
+var touch = { on: false, mode: 'PB' };
+
+kb.onNoteOn = function(note, velocity) {
+  console.log('Pressed note ' + note + ' with velocity ' + velocity);
+  octaveData[note] = octave;
+  output.sendMessage([0x90, 60 + 12 * octave + note, velocity]);
+}
+
+kb.onNoteOff = function(note, velocity) {
+  console.log('Released note ' + note);
+  output.sendMessage([0x80, 60 + 12 * octaveData[note] + note, velocity]);
+  octaveData[note] = undefined;
+}
+
+kb.onLeft = function(press) {
+  console.log((press ? 'Pressed' : 'Released') + ' left on D-Pad');
+  if (press)
+    octave -= octave > -5 ? 1 : 0;
+}
+
+kb.onRight = function(press) {
+  console.log((press ? 'Pressed' : 'Released') + ' right on D-Pad');
+  if (press)
+    octave += octave < 4 ? 1 : 0;
+}
+
+kb.onTouchOn = function(percentage) {
+  console.log('Touched touch strip at ' + percentage + '%');
+  if (touch.mode == 'PB')
+    output.sendMessage([0xE0, 0, (1.27 * percentage) | 0]);
+  else if (touch.mode == 'Mod')
+    output.sendMessage([0xB0, 1, (1.27 * percentage) | 0]);
+  touch.on = true;
+}
+
+kb.onTouchOff = function(percentage) {
+  console.log('Let go of touch strip');
+  if (touch.mode == 'PB')
+    output.sendMessage([0xE0, 0, 64]);
+  touch.on = false;
+}
+
+kb.onTouchButton = function(press) {
+  console.log((press ? 'Pressed' : 'Released') + ' touch strip button');
+  if (press && !touch.on)
+    touch.mode = touch.mode == 'PB' ? 'Mod' : 'PB';
+}
+
+console.log('Available MIDI output ports:');
+for (var i = 0; i < output.getPortCount(); i++)
+  console.log('\tPort ' + i + ': ' + output.getPortName(i));
+
+pickPort();
+function pickPort() {
+  var rl = readline.open();
+  rl.question('Which port should I send the MIDI data to? Port ', function(port) {
+    rl.close();
+    port = parseInt(port);
+    try { output.openPort(port); }
+    catch (e) {
+      console.log('Error opening port: ' + e.message);
+      rl.close();
+      pickPort();
+      return;
+    }
+    console.log('Now sending data to ' + output.getPortName(port) + '...\n');
+    console.log('While playing use left and right on the D-Pad to switch octaves, and press the '
+      + 'button next to the touch strip to toggle between pitch bend and modulation.\n');
+    kb.start();
+  });
+}
---
+++ rbkey.js
@@ -0,0 +1,108 @@
+var _ = require('underscore');
+var HID = require('node-hid');
+var util = require('util');
+
+_.mixin({
+  sum: function(obj) {
+    if (!_.isArray(obj) || obj.length == 0) return 0;
+    return _.reduce(obj, function(sum, n) {
+      return sum += n;
+    });
+  }
+});
+
+module.exports = function RBKeyboard() {
+  var devices = HID.devices();
+  var validDevs = _.filter(devices,
+    function(d) { return d.vendorId == 7085 && d.productId == 13104; });
+  if (!validDevs.length)
+    throw new Error('No usable devices found.');
+
+  var device = new HID.HID(validDevs[0].path);
+  var noteData = [];
+  for (var i = 0; i < 25; i++) noteData[i] = false;
+  var dpadData = {};
+  var dirs = { Left: 6, Right: 2, Up: 0, Down: 4 };
+  _.each(dirs, function(v, k) { dpadData[k] = false });
+  var touchData = 0;
+  var touchButtonData = false;
+
+  var processData = function(data) {
+    // Uncomment these lines to print the raw data, useful for development
+    //_.each(data, function(d) { util.print(d + ' '); });
+    //console.log();
+
+    var dpad = data[2];
+    _.each(dirs, function(dir, name) {
+      var pressed = dpad == dir;
+      if (dpadData[name] != pressed) {
+        this['on' + name](pressed);
+        dpadData[name] = pressed;
+      }
+    }, this);
+    
+    var touch = data[15];
+    if (touchData != touch) {
+      touch != 0 ? this.onTouchOn(touch) : this.onTouchOff();
+      touchData = touch;
+    }
+    
+    var touchButton = data[13] != 0;
+    if (touchButtonData != touchButton) {
+      this.onTouchButton(touchButton);
+      touchButtonData = touchButton;
+    }
+
+    var note = 0;
+    for (var i = 5; i <= 8; i++) {
+      note += data[i] << (8 * (8 - i));
+    }
+    data[8] = data[8] & 0x7F;
+    var velocityData = _.filter(data.slice(8, 13), function(v) { return v != 0; });
+    for (var i = 0; i < 25; i++) {
+      if (noteData[i] && velocityData.length) {
+        noteData[i] = velocityData[0];
+        velocityData = velocityData.slice(1);
+      }
+
+      var mask = 0x80000000 >>> i;
+      var noteOn = (note & mask) != 0;
+      if ((noteData[i] != 0) != noteOn) {
+        var velocity = 0;
+        if (noteOn) {
+          if (velocityData.length) {
+            velocity = velocityData[0];
+            velocityData = velocityData.slice(1);
+          }
+          else {
+            var curVelocities = _.chain(noteData).filter(function(v) { return v != 0; }).values();
+            velocity = curVelocities.sum().value() / curVelocities.value().length | 0;
+          }
+        }
+        noteOn ? this.onNoteOn(i, velocity) : this.onNoteOff(i, noteData[i]);
+        noteData[i] = velocity;
+      }
+    }
+  };
+
+  var onRead = _.bind(function(error, data) {
+    if (error) throw error;
+    processData.call(this, data);
+    device.read(onRead);
+  }, this);
+
+  this.start = function() {
+    device.read(onRead);
+  };
+  
+  this.onNoteOn =
+  this.onNoteOff =
+  this.onLeft =
+  this.onRight =
+  this.onUp =
+  this.onDown =
+  this.onTouchOn =
+  this.onTouchOff =
+  this.onTouchButton =
+    function() { };
+}
